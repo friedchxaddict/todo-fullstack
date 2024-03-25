53d715e551d924a69b107166c1533505ca33ab81
@adrianfontanilla <adrianfontanilla@gmail.com>
2024-03-24 22:28:36 -0500

    check routing and reroute to user profile

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -7,6 +7,7 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = ({ user }) => {
   if (!user) return null;
+  console.log(user);
   return (
     <div>
       <h1>Welcome back to your todo list {user.name}</h1>
