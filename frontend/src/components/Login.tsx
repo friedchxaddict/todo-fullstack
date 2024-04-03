3172feeb090b795ae54f640ee4e5f97bbe47214a
@adrianfontanilla <adrianfontanilla@gmail.com>
2024-04-02 21:03:43 -0500

    move login card to center

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { ChangeEvent, useState } from 'react';
-import { Heading, Button, Card, Input } from '@chakra-ui/react';
+import { Heading, Button, Card, Input, Flex } from '@chakra-ui/react';
 
 interface CustomError {
   message: string;
@@ -55,42 +55,48 @@ const Login: React.FC<Props> = ({ onLogin, navigate }) => {
   };
 
   return (
-    <Card
-      bg="#333333"
-      maxW="300px"
-      h="400px"
-      borderColor="#FFA07A"
-      borderWidth="4px"
-      borderRadius="lg">
-      <Heading as="h2" color="#EFEFEF">
-        Login
-      </Heading>
-      {error && typeof error === 'string' && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          placeholder="Username"
-          required
-          mb={3}
-        />
-        <Input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder="Password"
-          autoComplete="current-password"
-          required
-          mb={3}
-        />
-        <Button bg="#FFA07A" type="submit">
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      width="100vw">
+      <Card
+        bg="#333333"
+        maxW="300px"
+        h="400px"
+        borderColor="#FFA07A"
+        borderWidth="4px"
+        borderRadius="lg">
+        <Heading as="h2" color="#EFEFEF">
           Login
-        </Button>
-      </form>
-    </Card>
+        </Heading>
+        {error && typeof error === 'string' && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            placeholder="Username"
+            required
+            mb={3}
+          />
+          <Input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Password"
+            autoComplete="current-password"
+            required
+            mb={3}
+          />
+          <Button bg="#FFA07A" type="submit">
+            Login
+          </Button>
+        </form>
+      </Card>
+    </Flex>
   );
 };
 
